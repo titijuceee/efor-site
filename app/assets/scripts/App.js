0e74331abf23bfef8885e8dcc811e86fd2013317
Judit Titi <mlle.judith.nagy@gmail.com>
2020-10-24 15:17:28 +0200

    page loader test

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import '../styles/styles.css'
 import 'lazysizes'
+import PageLoader from './modules/PageLoader'
 import MobileMenu from './modules/MobileMenu'
 
+new PageLoader();
 new MobileMenu();
 
 if (module.hot) {
