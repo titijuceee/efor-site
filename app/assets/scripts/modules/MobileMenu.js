da4d1d2daf11e3e0c9b1005909af5b8c5dd36c59
Judit Titi <mlle.judith.nagy@gmail.com>
2020-10-24 13:26:59 +0200

    mobile-menu closing bug fixed

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -3,17 +3,24 @@ class MobileMenu {
     this.menuIcon = document.querySelector(".site-header__menu-icon")
     this.menuContent = document.querySelector(".site-header__menu-content")
     this.siteHeader = document.querySelector(".site-header")
+    this.menuItem = document.querySelectorAll(".primary-nav__item")
     this.events()
   }
 
   events() {
     this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+    this.menuItem.forEach(el => el.addEventListener("click", () => this.hideTheMenu()))
   }
 
   toggleTheMenu() {
     this.menuContent.classList.toggle("site-header__menu-content--is-visible")
     this.siteHeader.classList.toggle("site-header--is-expanded")
     this.menuIcon.classList.toggle("site-header__menu-icon--close-x")
+  }
+  hideTheMenu() {
+    this.menuContent.classList.remove("site-header__menu-content--is-visible")
+    this.siteHeader.classList.remove("site-header--is-expanded")
+    this.menuIcon.classList.remove("site-header__menu-icon--close-x")
   }
 }
 
